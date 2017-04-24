698b85af0536cacb8b0988fa0e57d24c812ac656
torleifg <torgravels@gmail.com>
2017-04-24 16:13:23 +0200

    Conditions added to handle NoneType

--- crawler/aktuelt/aktuelt/spiders/document_spider.py
+++ crawler/aktuelt/aktuelt/spiders/document_spider.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from aktuelt.items import Document
 
 class DocumentSpider(scrapy.Spider):
-    name = "document"
+    name = "documents"
 
     def start_requests(self):
         with open('index.json') as json_data:
@@ -25,9 +25,25 @@ class DocumentSpider(scrapy.Spider):
         item['title'] = response.meta['title']
         
         if soup.find(id="blog2016-panel"):
-            item['content'] = ' '.join([soup.find("div", class_="pane-node-summary").get_text(" ", strip=True), 
-                soup.find("div", class_="pane-node-body").get_text(" ", strip=True)])  		
+            ingress = soup.find("div", class_="pane-node-field-ingress")
+            body = soup.find("div", class_="pane-node-body")
+
+            if ingress is None:
+                item['content'] = body.get_text(" ", strip=True)
+            elif body is None:
+                item['content'] = ingress.get_text(" ", strip=True)
+            else:
+                item['content'] = ' '.join([ingress.get_text(" ", strip=True), body.get_text(" ", strip=True)])
+                
         elif soup.find(id="article-panel-2016"):
-            item['content'] = ' '.join([soup.find("div", class_="pane-node-field-intro-text").get_text(" ", strip=True), 
-                soup.find("div", class_="pane-node-field-text").get_text(" ", strip=True)])
+            ingress = soup.find("div", class_="pane-node-field-intro-text")
+            body = soup.find("div", class_="pane-node-field-text")
+            
+            if ingress is None:
+                item['content'] = body.get_text(" ", strip=True)
+            elif body is None:
+                item['content'] = ingress.get_text(" ", strip=True)
+            else:
+                item['content'] = ' '.join([ingress.get_text(" ", strip=True), body.get_text(" ", strip=True)])
+
         return item
